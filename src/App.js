c16eb2bcb14d13105d9e7301a9a4d24dd4eeea55
Mario Senese <senese.mario90@gmail.com>
2023-03-20 23:26:11 +0100

    Adding logo & update files

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ function App(props) {
       <Helmet>
         <html lang='it'/>
         <title>Mario Senese</title>
-        <meta name='description' content='Mario Senese | Developer'/>
-        <meta name='theme-color' content='#111111'/>
-        <meta name='keywords' content='mario senese, developer, programmatore' />
+	<meta name="description" content="Mario Senese | Developer"/>        
+        <meta name="theme-color" content="#111111"/>
+        <meta name="keywords" content="mario senese, developer, programmatore" />
       </Helmet>
 
       <header className="wrapper">
-        <h1 className="glitch" data-text={props.name}>
+         <img className="glitch" data-src="./images/logo/Logo-3.png" src="./images/logo/Logo-3.png" alt="Mario Senese" width="150" height="150" />
+	 <h1 className="glitch" data-text={props.name}>
           {props.name}
         </h1>
         <span className="sub">
